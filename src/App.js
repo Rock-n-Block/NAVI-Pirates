707386336fb2617c152529e041f1b0be3cf9d284
Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
2020-12-07 16:52:22 +0300

    binance login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 import React from "react";
-import {MainPage} from "./pages";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+import { MainPage } from "./pages";
+import BinanceService from './utils/binance';
+import { userActions } from './redux/actions';
+
 import './styles/main.scss'
 
 function App() {
+  const [binanceService, setBinanceService] = React.useState(null)
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    let counter = 0;
+
+    const interval = setInterval(() => {
+      counter += 10;
+      if (window['BinanceChain']) {
+        clearInterval(interval)
+
+        const binance = new BinanceService()
+
+        setBinanceService(binance)
+
+        binance.getAccount().then(res => {
+          dispatch(userActions.setUserData(res))
+          // dispatch(modalActions.toggleModal(false))
+        }).catch(err => {
+          console.log(err)
+          dispatch(userActions.setUserData(err))
+          // dispatch(modalActions.toggleModal(true))
+        })
+      } else if (counter > 3000) {
+        console.log('error, binanceChain')
+      }
+    }, 10)
+  }, [])
   return (
 
-      <Route exact path="/" render={() =>
-          <MainPage />}
-      />
+    <Route exact path="/" render={() =>
+      <MainPage binanceService={binanceService} />}
+    />
 
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { userActions } from '../../redux/actions';
 
 import 'swiper/swiper.scss';
 import './header.scss';
@@ -14,9 +16,10 @@ import trust_img from '../../assets/img/trust.png'
 import arrowPrevImg from '../../assets/img/prev.png'
 import arrowNextImg from '../../assets/img/next.png'
 
-function Header() {
+function Header({ binanceService }) {
+    const dispatch = useDispatch()
 
-    const [isTabActive, setIsTabActive]  = React.useState(false);
+    const [isTabActive, setIsTabActive] = React.useState(false);
     const [isSlidesActive, setSlidesActive] = React.useState(false);
 
 
@@ -36,14 +39,21 @@ function Header() {
 
     let count = 4;
 
-    const reload = () =>{
+    const reload = () => {
         window.location.reload();
     };
 
-    const up = () =>{
+    const up = () => {
         document.documentElement.scrollTop = 0;
     };
 
+    const handleLogin = () => {
+        binanceService.getAccount()
+            .then(res => dispatch(userActions.setUserData(res)))
+            .catch(err => dispatch(userActions.setUserData(err)))
+    }
+
+    const userAddress = useSelector(({ user }) => user.address)
 
     //hook for function
     React.useEffect(() => {
@@ -57,115 +67,115 @@ function Header() {
     }, []);
 
     return (
-    <div className="header">
-        <div className = "row">
-            <div className = "header__content">
-              <div className = "header__left">
-                <NavLink to = "/">
-                    <img src = {logo} alt = ""  onClick={reload}/>
-                    {isTabActive?(
-                        <img className="tab" src = {tab} alt = ""  onClick={up}/>
-                    ):
-                        (null)}
-                </NavLink>
+        <div className="header">
+            <div className="row">
+                <div className="header__content">
+                    <div className="header__left">
+                        <NavLink to="/">
+                            <img src={logo} alt="" onClick={reload} />
+                            {isTabActive ? (
+                                <img className="tab" src={tab} alt="" onClick={up} />
+                            ) :
+                                (null)}
+                        </NavLink>
 
-              </div>
-                <div className = "header__right">
-                   <button className = "header__right-login-button">
-                     <img src={login_img}/>
-                       <div>Login to Binance Smart Chain Wallet</div>
-                   </button>
-                    <button className = "header__right-login-1-button">
-                        <img src={trust_img}/>
-                        <div>Trust Wallet</div>
-                    </button>
-
-                    <button className = "header__right-nftCard-button" onClick={()=>{
-                        setSlidesActive(!isSlidesActive)
-                    }}>
-                        <div> My VIP NFT CARD </div>
-                        <div className = "count-component">
-                            <div className = "count-value">
-                                {count}
-                            </div>
-                        </div>
-                    </button>
-                    <button className = "header__right-pawCard-button">
-                        BUY VIP PAW CARD
-                        <div className = "pawCard-count-component-down" onClick={()=>{
-                            document.getElementById("pawCard-panel").style.display = "block";
-                            document.getElementsByClassName("pawCard-count-component-up")[0].style.display = "block";
-                            document.getElementsByClassName("pawCard-count-component-down")[0].style.display = "none"
-
-                        }}>
-                        </div>
-                        <div className = "pawCard-count-component-up" onClick={()=>{
-                            document.getElementById("pawCard-panel").style.display = "";
-                            document.getElementsByClassName("pawCard-count-component-down")[0].style.display = "block";
-                            document.getElementsByClassName("pawCard-count-component-up")[0].style.display = "none";
-                        }}>
-                        </div>
-                    </button>
-                    <div className = "header__right-pawCard-panel" id = "pawCard-panel">
-                        <div className="header__right-pawCard-panel-counter">
-                            <button>-</button>
-                            <div className="header__right-pawCard-panel-value">
-                                20
-                            </div>
-                            <button>+</button>
-                        </div>
-                        <div className="header__right-pawCard-panel-cost">
-                            $ 200
-                        </div>
-                        <button className="header__right-pawCard-panel-buy-button">BUY CARD</button>
                     </div>
-                </div>
-                {isSlidesActive ? (
-                    <div className = "header__swiper" id = "swiper">
-                        <Swiper
-                            spaceBetween={30}
-                            slidesPerView={1}
+                    <div className="header__right">
+                        {!userAddress && <button className="header__right-login-button" onClick={handleLogin}>
+                            <img src={login_img} />
+                            <div>Login to Binance Smart Chain Wallet</div>
+                        </button>}
+                        <button className="header__right-login-1-button">
+                            <img src={trust_img} />
+                            <div>Trust Wallet</div>
+                        </button>
+
+                        <button className="header__right-nftCard-button" onClick={() => {
+                            setSlidesActive(!isSlidesActive)
+                        }}>
+                            <div> My VIP NFT CARD </div>
+                            <div className="count-component">
+                                <div className="count-value">
+                                    {count}
+                                </div>
+                            </div>
+                        </button>
+                        <button className="header__right-pawCard-button">
+                            BUY VIP PAW CARD
+                        <div className="pawCard-count-component-down" onClick={() => {
+                                document.getElementById("pawCard-panel").style.display = "block";
+                                document.getElementsByClassName("pawCard-count-component-up")[0].style.display = "block";
+                                document.getElementsByClassName("pawCard-count-component-down")[0].style.display = "none"
+
+                            }}>
+                            </div>
+                            <div className="pawCard-count-component-up" onClick={() => {
+                                document.getElementById("pawCard-panel").style.display = "";
+                                document.getElementsByClassName("pawCard-count-component-down")[0].style.display = "block";
+                                document.getElementsByClassName("pawCard-count-component-up")[0].style.display = "none";
+                            }}>
+                            </div>
+                        </button>
+                        <div className="header__right-pawCard-panel" id="pawCard-panel">
+                            <div className="header__right-pawCard-panel-counter">
+                                <button>-</button>
+                                <div className="header__right-pawCard-panel-value">
+                                    20
+                            </div>
+                                <button>+</button>
+                            </div>
+                            <div className="header__right-pawCard-panel-cost">
+                                $ 200
+                        </div>
+                            <button className="header__right-pawCard-panel-buy-button">BUY CARD</button>
+                        </div>
+                    </div>
+                    {isSlidesActive ? (
+                        <div className="header__swiper" id="swiper">
+                            <Swiper
+                                spaceBetween={30}
+                                slidesPerView={1}
                             // effect="fade"
-                         //   onSwiper={swiper => setSwiper(swiper)}
+                            //   onSwiper={swiper => setSwiper(swiper)}
                             //onSlideChangeTransitionEnd={handleSlideChange}
 
-                        >
-                            <SwiperSlide>
-                                <div className="swiper-slide-data">
-                                    <div className="swiper-slide-value">
-                                        count 1
+                            >
+                                <SwiperSlide>
+                                    <div className="swiper-slide-data">
+                                        <div className="swiper-slide-value">
+                                            count 1
                                     </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="swiper-slide-data">
-                                    <div className="swiper-slide-value">
-                                        count 2
                                     </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="swiper-slide-data">
-                                    <div className="swiper-slide-value">
-                                        count 3
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="swiper-slide-data">
+                                        <div className="swiper-slide-value">
+                                            count 2
                                     </div>
-                                </div>
-                            </SwiperSlide>
-                        </Swiper>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="swiper-slide-data">
+                                        <div className="swiper-slide-value">
+                                            count 3
+                                    </div>
+                                    </div>
+                                </SwiperSlide>
+                            </Swiper>
 
-                    <div className="media__btn media__btn--prev" onClick={() => goPrev()}>
-                        <img src={arrowPrevImg} alt="" />
-                    </div>
-                    <div className="media__btn media__btn--next" onClick={() => goNext()}>
-                        <img src={arrowNextImg} alt="" />
-                    </div>
+                            <div className="media__btn media__btn--prev" onClick={() => goPrev()}>
+                                <img src={arrowPrevImg} alt="" />
+                            </div>
+                            <div className="media__btn media__btn--next" onClick={() => goNext()}>
+                                <img src={arrowNextImg} alt="" />
+                            </div>
 
-                </div>): null}
+                        </div>) : null}
 
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
